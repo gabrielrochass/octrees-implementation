e1eb7f5e233c7fb482a1abd40025f6cb5633a62a
gabrielrochass <120466775+gabrielrochass@users.noreply.github.com>
2024-07-26 12:05:06 -0300

    feat: plano do projeto

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # octrees-implementation
 Projeto da disciplina de Processamento Gráfico de 2024.1 do Centro de Informática da UFPE.
+ ## Definição
+ - Octrees são uma estrutura de dados hierárquica usada para dividir um espaço tridimensional em partes menores, chamadas de "octantes". Cada nó do octree divide o espaço em oito sub-volumes menores. Isso é útil para acelerar operações como colisão, renderização e busca espacial em aplicações 3D.
+ 
---
+++ main.cpp
@@ -0,0 +1,15 @@
+// octrees implementation
+
+// define estrutura do nó, referenciando os 8 filhos e uma bounding box que delimita o espaço coberto pelo nó
+
+// método de divisão do espaço em 8 partes iguais, a partir do centro da bounding box
+
+// método de inserção de um ponto no octree em um nó (sub-volume) específico -> se ligar no momento em que um novo nó é criado (critério de subdivisão: quantidade de pontos no nó > 1)
+
+// método de busca de um ponto no octree
+
+// método de remoção de um ponto no octree
+
+// método de visualização do octree
+
+// método de visualização do octree em formato de árvore
